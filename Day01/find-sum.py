f31f2ec9bd739ccf34b1ebca4b66c588f40dccb1
Jason Graham <jason.m.graham@boeing.com>
2020-12-31 00:18:36 -0700

    Completed some documentation of the functions and fixed a bug in the validate_num function.   There was a logic error in the ivalue compare.

--- Day01/find-sum.py
+++ Day01/find-sum.py
@@ -9,12 +9,13 @@ def find_sum(filename: str, target_sum: int, num_to_total: int) -> None:
 
   Description:  This function will open a file and scan it, looking for a pair
                 of numbers whose sum is equal to target_sum.  It will then return
-                 the product of those two numbers.
+                the product of those two numbers.
 
   Input:        filename: str - Input filename
 
-                target_sum: int - total that numbers should sum to.
+                target_sum: int - Sum total that we are searching for in the report.
 
+                num_to_total: int - The number of values to use in calculating the sum.
 
   Outputs:      None
   """
@@ -49,9 +50,18 @@ def find_sum(filename: str, target_sum: int, num_to_total: int) -> None:
 
 # ------------------------------------------------------------------------------
 def validate_num(value: int) -> int:
+  """ Validate the value passed in with the --num option.
+
+  Description:  Validates the --num option values.  The value must be between
+                2 and 10 (arbitrarily bounded upper limit.
+
+  Input:        value: int - Value to be validated.
+
+  Outputs:      int - Validated value
+  """
 
   ivalue = int(value)
-  if 2 > ivalue < 2 or 10 <= ivalue:
+  if 2 > ivalue or 10 < ivalue:
     raise argparse.ArgumentTypeError(value, 'is an invalid option.  Must be an integer between 2 and 10')
   return ivalue
 
