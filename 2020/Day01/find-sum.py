8dab948f5ad54f055976ff6330d80c14c5f64960
Jason Graham <jason.m.graham@gmail.com>
2023-12-09 16:43:57 -0700

    Moved 2020 solutions to a s year specific directory.

--- 2020/Day01/find-sum.py
+++ 2020/Day01/find-sum.py
@@ -8,14 +8,16 @@ def find_sum(filename: str, target_sum: int, num_to_total: int) -> None:
   """ Find a specified number of values that sum to the total provided and calculate the product of them.
 
   Description:  This function will open a file and scan it, looking for a pair
-                of numbers whose sum is equal to target_sum.  It will then return
-                the product of those two numbers.
+                of numbers whose sum is equal to target_sum.  It will then
+                return the product of those two numbers.
 
   Input:        filename: str - Input filename
 
-                target_sum: int - Sum total that we are searching for in the report.
+                target_sum: int - Sum total that we are searching for in the
+                                  report.
 
-                num_to_total: int - The number of values to use in calculating the sum.
+                num_to_total: int - The number of values to use in calculating
+                                    the sum.
 
   Outputs:      None
   """
@@ -62,7 +64,8 @@ def validate_num(value: int) -> int:
 
   ivalue = int(value)
   if 2 > ivalue or 10 < ivalue:
-    raise argparse.ArgumentTypeError(value, 'is an invalid option.  Must be an integer between 2 and 10')
+    raise argparse.ArgumentTypeError(value, 'is an invalid option.  Must be an \
+                                     integer between 2 and 10')
   return ivalue
 
 
@@ -71,8 +74,10 @@ if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(description='Advent Of Code Challenge 2020 - Day 1, Find 2 numbers that sum 2020 and output the product of the two.')
   parser.add_argument('file_in', type=str, metavar='input_filename', help='Name of input file.')
-  parser.add_argument('--sum', '-s', type=int, metavar='sum', default='2020', help='Alternate sum value (default is 2020).')
-  parser.add_argument('--num', '-n', type=validate_num, metavar='number', default='2', help='Number of values to sum`')
+  parser.add_argument('--sum', '-s', type=int, metavar='sum', default='2020', \
+                      help='Alternate sum value (default is 2020).')
+  parser.add_argument('--num', '-n', type=validate_num, metavar='number', \
+                      default='2', help='Number of values to sum`')
 
   args = parser.parse_args()
 
